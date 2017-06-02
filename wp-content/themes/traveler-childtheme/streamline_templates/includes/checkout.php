901ca57cf50d9485710e8a599e1ec8591c12377a
Valery Kuzivanov <valery.kuzivanov@sibers.com>
2017-06-02 20:07:31 +0600

    template added

--- wp-content/themes/traveler-childtheme/streamline_templates/includes/checkout.php
+++ wp-content/themes/traveler-childtheme/streamline_templates/includes/checkout.php
@@ -1,7 +1,10 @@
 <h1>Checkout</h1>
 <form name="checkout-form" novalidate>
     <div class="row">
-        <h3 class="border-bottom">Personal information</h3>
+        <div class="col-md-12">
+            <h5 class="border-bottom">Personal information</h5>
+        </div>
+
         <div class="form-group col-md-6">
             <label for="checkout-first-name">First name:</label>
             <input type="text" class="form-control" id="checkout-first-name" name="checkout-first-name" placeholder="First name">
@@ -16,10 +19,13 @@
         </div>
         <div class="form-group col-md-6">
             <label for="checkout-phone">Phone:</label>
-            <input type="text" class="form-control" id="checkout-phone" name="checkout-phone" placeholder="Phone">
+            <input type="text" class="form-control" id="checkout-phone" name="checkout-phone" placeholder="(###) ###-####">
         </div>
 
-        <h3 class="border-bottom">Payment information</h3>
+        <div class="col-md-12">
+            <h5 class="border-bottom">Payment information</h5>
+        </div>
+
         <div class="form-group col-md-4">
             <label for="checkout-adress">Adress:</label>
             <input type="text" class="form-control" id="checkout-adress" name="checkout-adress" placeholder="Adress name" required>
@@ -31,46 +37,45 @@
         <div class="form-group col-md-4">
             <label for="checkout-country">Country:</label>
             <select class="form-control" type="text" class="form-control" id="checkout-country" name="checkout-country">
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-                <option>4</option>
-                <option>5</option>
+                <option>United states</option>
+                <option>United states</option>
+                <option>Canada</option>
+                <option>Canada</option>
             </select>
         </div>
 
         <div class="form-group col-md-6">
-            <label for="checkout-first-name">First name</label>
-            <input type="text" class="form-control" id="checkout-first-name" name="checkout-first-name" placeholder="First name">
+            <label for="checkout-first-name">City:</label>
+            <input type="text" class="form-control" id="checkout-city" name="checkout-city" placeholder="City">
         </div>
         <div class="form-group col-md-4">
-            <label for="checkout-first-name">First name</label>
-            <input type="text" class="form-control" id="checkout-first-name" name="checkout-first-name" placeholder="First name">
+            <label for="checkout-first-name">State:</label>
+            <input type="text" class="form-control" id="checkout-state" name="checkout-state" placeholder="State">
         </div>
         <div class="form-group col-md-2">
-            <label for="checkout-first-name">First name</label>
-            <input type="text" class="form-control" id="checkout-first-name" name="checkout-first-name" placeholder="First name">
+            <label for="checkout-first-name">Postal code:</label>
+            <input type="text" class="form-control" id="checkout-postal-code" name="checkout-postal-code" placeholder="Postal code">
         </div>
 
         <div class="form-group col-md-3">
-            <label for="checkout-first-name">First name</label>
-            <input type="text" class="form-control" id="checkout-first-name" name="checkout-first-name" placeholder="First name">
+            <label for="checkout-first-name">Cart Type:</label>
+            <input type="text" class="form-control" id="checkout-card-type" name="checkout-card-type" placeholder="Cart Type">
         </div>
         <div class="form-group col-md-3">
-            <label for="checkout-first-name">First name</label>
-            <input type="text" class="form-control" id="checkout-first-name" name="checkout-first-name" placeholder="First name">
+            <label for="checkout-first-name">Card number:</label>
+            <input type="text" class="form-control" id="checkout-card-number" name="checkout-card-number" placeholder="Card number">
         </div>
         <div class="form-group col-md-2">
-            <label for="checkout-first-name">First name</label>
-            <input type="text" class="form-control" id="checkout-first-name" name="checkout-first-name" placeholder="First name">
+            <label for="checkout-first-name">Exp Month:</label>
+            <input type="number" class="form-control" id="checkout-exp-month" name="checkout-exp-month" placeholder="Exp Month">
         </div>
         <div class="form-group col-md-2">
-            <label for="checkout-first-name">First name</label>
-            <input type="text" class="form-control" id="checkout-first-name" name="checkout-first-name" placeholder="First name">
+            <label for="checkout-first-name">Exp year:</label>
+            <input type="number" class="form-control" id="checkout-exp-year" name="checkout-exp-year" placeholder="Exp year">
         </div>
         <div class="form-group col-md-2">
-            <label for="checkout-first-name">First name</label>
-            <input type="text" class="form-control" id="checkout-first-name" name="checkout-first-name" placeholder="First name">
+            <label for="checkout-first-name">CVV:</label>
+            <input type="text" class="form-control" id="checkout-cvv" name="checkout-cvv" placeholder="CVV">
         </div>
 
     </div>
