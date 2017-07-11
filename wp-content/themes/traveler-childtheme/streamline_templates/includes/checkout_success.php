b7b1ee0ffe3bfb7470dc4a4ff81045fc870faa47
kostya <dispector312@gmail.com>
2017-07-11 13:52:56 +0600

    fix price showing

--- wp-content/themes/traveler-childtheme/streamline_templates/includes/checkout_success.php
+++ wp-content/themes/traveler-childtheme/streamline_templates/includes/checkout_success.php
@@ -5,7 +5,18 @@
             <tbody>
                 <tr ng-repeat="(prop, val) in successCheckoutData">
                     <td>{[prop]}</td>
-                    <td>{[val]}</td>
+
+                    <td ng-switch on="prop">
+                        <span ng-switch-when="price_common">
+                            {[val | currency: "$" : 2 ]}
+                        </span>
+                        <span ng-switch-when="price_balance">
+                            {[val | currency: "$" : 2 ]}
+                        </span>
+                        <span ng-switch-default>
+                          {[val]}
+                        </span>
+                    </td>
                 </tr>
             </tbody>
         </table>
